4696be31b8b074bf96e921e6091e24e7d550dacc
perodinamit <matej.curcic@gmail.com>
2016-04-29 14:51:16 +0200

    Read from XML and to XML

---
+++ XMLintoTxt/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace XMLintoTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnWritetoXML = new System.Windows.Forms.Button();
+            this.loadXML = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Address";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(68, 10);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 20);
+            this.name.TabIndex = 1;
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(68, 41);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(100, 20);
+            this.age.TabIndex = 1;
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(68, 72);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(100, 20);
+            this.address.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(367, 230);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnWritetoXML
+            // 
+            this.btnWritetoXML.Location = new System.Drawing.Point(261, 8);
+            this.btnWritetoXML.Name = "btnWritetoXML";
+            this.btnWritetoXML.Size = new System.Drawing.Size(133, 23);
+            this.btnWritetoXML.TabIndex = 3;
+            this.btnWritetoXML.Text = "From textbox to XML";
+            this.btnWritetoXML.UseVisualStyleBackColor = true;
+            this.btnWritetoXML.Click += new System.EventHandler(this.btnWritetoXML_Click);
+            // 
+            // loadXML
+            // 
+            this.loadXML.Location = new System.Drawing.Point(261, 44);
+            this.loadXML.Name = "loadXML";
+            this.loadXML.Size = new System.Drawing.Size(133, 23);
+            this.loadXML.TabIndex = 3;
+            this.loadXML.Text = "XML to Textbox";
+            this.loadXML.UseVisualStyleBackColor = true;
+            this.loadXML.Click += new System.EventHandler(this.loadXML_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "XML preview";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Preview";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 364);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loadXML);
+            this.Controls.Add(this.btnWritetoXML);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnWritetoXML;
+        private System.Windows.Forms.Button loadXML;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ XMLintoTxt/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XMLintoTxt
+{
+    
+
+    public partial class Form1 : Form
+    {
+        private XmlDocument doc;
+        private XmlElement root;
+        public string PATH = @"C:\temp\Person.xml";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWritetoXML_Click(object sender, EventArgs e)
+        {
+            XmlWriter xmlWriter = XmlWriter.Create(PATH);
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("Person");
+
+            xmlWriter.WriteStartElement("Name");
+            xmlWriter.WriteString(name.Text);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("Age");
+            xmlWriter.WriteString(age.Text);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("Address");
+            xmlWriter.WriteString(address.Text);
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteEndDocument();
+
+            xmlWriter.Close();
+        }
+
+        private void loadXML_Click(object sender, EventArgs e)
+        {
+            doc = new XmlDocument();
+            doc.Load(PATH);
+            root = doc.DocumentElement;
+            name.Text = root.GetElementsByTagName("Name")[0].InnerText;
+            age.Text = root.GetElementsByTagName("Age")[0].InnerText;
+            address.Text = root.GetElementsByTagName("Address")[0].InnerText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.LoadFile(PATH, RichTextBoxStreamType.PlainText);
+        }
+    }
+}
